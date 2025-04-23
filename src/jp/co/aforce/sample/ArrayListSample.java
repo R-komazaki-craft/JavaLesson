647f2a319f3116c42e8d2b8f069b8a74b3cec2fd
runakomazaki <runakomazaki@craft-net.co.jp>
2025-04-23 16:10:08 +0900

    aaa

--- src/jp/co/aforce/sample/ArrayListSample.java
+++ src/jp/co/aforce/sample/ArrayListSample.java
@@ -19,29 +19,8 @@ public class ArrayListSample {
 		fruitsList.remove("peach");
 
 		System.out.println(fruitsList);
-
 	}
-
 }
 
 
 
-
-
-
-
-
-
-
-
-//問１
-		List<String> countList = new ArrayList<String>();
-		
-		countList.add("0");
-		countList.add("1");
-		countList.add("2");
-		countList.add("3");
-		countList.add("4");
-	
-		String element = (4,"")
-		
