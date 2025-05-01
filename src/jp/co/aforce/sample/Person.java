5df794966dda4a951cd490c7be9878bc3c4d321a
runakomazaki <runakomazaki@craft-net.co.jp>
2025-05-01 09:39:20 +0900

    0501

--- src/jp/co/aforce/sample/Person.java
+++ src/jp/co/aforce/sample/Person.java
@@ -1,8 +1,17 @@
 package jp.co.aforce.sample;
 
 public class Person {
-	public static void main(String[] args) {
-		PersonMain
+	String name;
+	int age;
+	
+	//コンストラクタ
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
 	}
-
+	
+	public static void main(String[]args) {
+		Person p = new Person("太郎",25);
+		System.out.println("名前;" + p.name + "年齢:" + p.age);
+	}
 }
